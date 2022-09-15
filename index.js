9daba0f7e73f7ca6f6e97e069c8c127bf61d2ae4
Kevin Irungu <kevin.irungu@student.moringaschool.com>
2022-09-15 19:21:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const copyArray = word.split("");
+
+  const reversedCopyArray = copyArray.reverse();
+
+  const jointReversedArray = reversedCopyArray.join("");
+
+  if(jointReversedArray === word) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
-  Add your pseudocode here
+Here's the logic:
+
+1. Take a copy of the string and split it into many strings, and put them in an array(.split())
+2. Reverse that array
+3. Join the reveresed array using .join() method 
+4. Compare the resulting string with the original string
 */
 
 /*
   Add written explanation of your solution here
 */
+isPalindrome("madam");
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
